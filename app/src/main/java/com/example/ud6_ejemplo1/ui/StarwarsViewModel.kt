0cbd0e5652476840070941e1f5510538c0747285
User <user@es.es>
2024-12-02 06:25:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ud6_ejemplo1/ui/StarwarsViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.ud6_ejemplo1.ui
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ud6_ejemplo1.conexion.Api
+import com.example.ud6_ejemplo1.modelo.Respuesta
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed interface StarwarsUIState {
+    data class Exito(val respuesta: Respuesta) : StarwarsUIState
+    object Error : StarwarsUIState
+    object Cargando : StarwarsUIState
+}
+
+class StarwarsViewModel : ViewModel() {
+    var starwarsUIState: StarwarsUIState by mutableStateOf(StarwarsUIState.Cargando)
+        private set
+
+    init {
+        obtenerPersonaje()
+    }
+
+    fun obtenerPersonaje() {
+        viewModelScope.launch {
+            starwarsUIState = StarwarsUIState.Cargando
+            starwarsUIState = try {
+                val listaPersonajes = Api.servicioRetrofit.obtenerPersonaje()
+                StarwarsUIState.Exito(listaPersonajes)
+            } catch (e: IOException){
+                StarwarsUIState.Error
+            } catch (e: HttpException){
+                StarwarsUIState.Error
+            }
+        }
+    }
+}
